3180390ce55d63c27549e7783411fcf9047d3ef6
MarioA <maestradaz3@gmail.com>
2021-10-19 11:54:54 -0500

    imagenes entrega

--- src/Repository/EntregaRepository.php
+++ src/Repository/EntregaRepository.php
@@ -9,17 +9,20 @@ use App\Entity\Entrega;
 use App\Entity\Panal;
 use App\Entity\Usuario;
 use App\Utilidades\Firebase;
+use App\Utilidades\SpaceDO;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 class EntregaRepository extends ServiceEntityRepository
 {
     private $firebase;
+    private $space;
 
-    public function __construct(ManagerRegistry $registry, Firebase $firebase)
+    public function __construct(ManagerRegistry $registry, Firebase $firebase, SpaceDO $space)
     {
         parent::__construct($registry, Entrega::class);
         $this->firebase = $firebase;
+        $this->space = $space;
     }
 
     public function apiLista($codigoCelda)
