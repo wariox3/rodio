ddaa7d3aac2ccdfe3ecebfdf8771707794a2655f
MarioA <maestradaz@gmail.com>
2021-12-20 09:57:23 -0500

    Limpiar base de datos

---
+++ recursos/limpiar.sql
@@ -0,0 +1,13 @@
+DELETE FROM votacion_celda;
+DELETE FROM votacion_detalle;
+DELETE FROM votacion;
+DELETE FROM reserva_item;
+DELETE FROM reserva;
+DELETE FROM contenido;
+DELETE FROM soporte;
+DELETE FROM atencion;
+DELETE FROM visita;
+DELETE FROM celda_usuario;
+DELETE FROM usuario;
+DELETE FROM entrega;
+DELETE FROM celda;
