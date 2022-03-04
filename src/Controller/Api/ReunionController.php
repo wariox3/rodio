10672abd67c9e17f71366a8ff91a6a0319c360ba
MarioA <maestradaz3@gmail.com>
2022-03-03 19:45:36 -0500

    feat: Varios url

---
+++ src/Controller/Api/ReunionController.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace App\Controller\Api;
+
+use App\Entity\Reunion;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ReunionController extends AbstractFOSRestController
+{
+
+    /**
+     * @Rest\Post("/api/admin/reunion/lista")
+     */
+    public function adminLista(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $raw = json_decode($request->getContent(), true);
+        $codigoPanal = $raw['codigoPanal']?? null;
+        if($codigoPanal) {
+            return $em->getRepository(Reunion::class)->apiAdminLista($codigoPanal);
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'Faltan parametros para el consumo de la api'
+            ];
+        }
+    }
+
+    /**
+     * @Rest\Post("/api/admin/reunion/nuevo")
+     */
+    public function adminNuevo(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $raw = json_decode($request->getContent(), true);
+        $id = $raw['id']?? null;
+        $codigoPanal = $raw['codigoPanal']?? null;
+        $nombre = $raw['nombre']?? null;
+        if($codigoPanal && $nombre) {
+            return $em->getRepository(Reunion::class)->apiAdminNuevo($codigoPanal, $id, $nombre);
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'Faltan parametros para el consumo de la api'
+            ];
+        }
+    }
+
+    /**
+     * @Rest\Post("/api/admin/reunion/detalle")
+     */
+    public function adminDetalle(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $raw = json_decode($request->getContent(), true);
+        $codigoReunion = $raw['codigoReunion']?? null;
+        if($codigoReunion) {
+            return $em->getRepository(Reunion::class)->apiAdminDetalle($codigoReunion);
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'Faltan parametros para el consumo de la api'];
+        }
+    }
+
+    /**
+     * @Rest\Post("/api/admin/reunion/detallenuevo")
+     */
+    public function adminDetalleNuevo(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $raw = json_decode($request->getContent(), true);
+        $id = $raw['id']?? null;
+        $descripcion = $raw['descripcion']?? null;
+        if($id && $descripcion) {
+            return $em->getRepository(Reunion::class)->apiAdminDetalleNuevo($id, $descripcion);
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'Faltan parametros para el consumo de la api'
+            ];
+        }
+    }
+
+    /**
+     * @Rest\Post("/api/admin/reunion/detalleeliminar")
+     */
+    public function adminDetalleEliminar(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $raw = json_decode($request->getContent(), true);
+        $id = $raw['id']?? null;
+        if($id) {
+            return $em->getRepository(Reunion::class)->apiAdminDetalleEliminar($id);
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'Faltan parametros para el consumo de la api'
+            ];
+        }
+    }
+
+    /**
+     * @Rest\Post("/api/admin/reunion/cerrar")
+     */
+    public function adminCerrar(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $raw = json_decode($request->getContent(), true);
+        $codigoReunion = $raw['codigoReunion']?? null;
+        if($codigoReunion) {
+            return $em->getRepository(Reunion::class)->apiAdminCerrar($codigoReunion);
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'Faltan parametros para el consumo de la api'];
+        }
+    }
+}
--- src/Repository/ReunionRepository.php
+++ src/Repository/ReunionRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Caso;
 use App\Entity\CasoTipo;
+use App\Entity\Panal;
 use App\Entity\Reunion;
+use App\Entity\ReunionDetalle;
 use App\Entity\Usuario;
 use App\Utilidades\Firebase;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -20,4 +22,99 @@ class ReunionRepository  extends ServiceEntityRepository
         $this->firebase = $firebase;
     }
 
+    public function apiAdminLista($codigoPanal)
+    {
+        $em = $this->getEntityManager();
+        $queryBuilder = $em->createQueryBuilder()->from(Reunion::class, 'r')
+            ->select('r.codigoReunionPk')
+            ->addSelect('r.fecha')
+            ->addSelect('r.nombre')
+            ->addSelect('r.cantidad')
+            ->addSelect('r.cantidadCoeficiente')
+            ->addSelect('r.estadoCerrado')
+            ->where("r.codigoPanalFk = {$codigoPanal}")
+            ->orderBy('r.fecha', 'DESC');
+        $arReuniones = $queryBuilder->getQuery()->getResult();
+        return [
+            'error' => false,
+            'reuniones' => $arReuniones
+        ];
+
+    }
+
+    public function apiAdminNuevo($codigoPanal, $id, $nombre)
+    {
+        $em = $this->getEntityManager();
+        $arPanal = $em->getRepository(Panal::class)->find($codigoPanal);
+        if($arPanal) {
+            if($id) {
+                $arReunion = $em->getRepository(Reunion::class)->find($id);
+                if($arReunion) {
+                    $arReunion->setNombre($nombre);
+                    $em->persist($arReunion);
+                    $em->flush();
+                    return [
+                        'error' => false,
+                        'codigoReunion' => $arReunion->getCodigoReunionPk()
+                    ];
+                } else {
+                    return [
+                        'error' => true,
+                        'errorMensaje' => "el id de reunion no existe"
+                    ];
+                }
+            } else {
+                $arReunion = new Reunion();
+                $arReunion->setPanalRel($arPanal);
+                $arReunion->setFecha(new \DateTime('now'));
+                $arReunion->setNombre($nombre);
+                $em->persist($arReunion);
+                $em->flush();
+                return [
+                    'error' => false,
+                    'codigoReunion' => $arReunion->getCodigoReunionPk()
+                ];
+            }
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => "No existe el panal"
+            ];
+        }
+    }
+
+    public function apiAdminDetalle($codigoReunion)
+    {
+        $em = $this->getEntityManager();
+        $queryBuilder = $em->createQueryBuilder()->from(Reunion::class, 'r')
+            ->select('r.codigoReunionPk')
+            ->addSelect('r.fecha')
+            ->addSelect('r.nombre')
+            ->addSelect('r.cantidad')
+            ->addSelect('r.cantidadCoeficiente')
+            ->addSelect('r.estadoCerrado')
+            ->addSelect('p.coeficiente as panalCoeficiente')
+            ->addSelect('p.area as panalArea')
+            ->addSelect('p.cantidad as panalCantidad')
+            ->leftJoin('r.panalRel', 'p')
+            ->where("r.codigoReunionPk = {$codigoReunion}");
+        $arReunion = $queryBuilder->getQuery()->getResult();
+        if($arReunion) {
+            $queryBuilder = $em->createQueryBuilder()->from(ReunionDetalle::class, 'rd')
+                ->select('rd.codigoReunionDetallePk')
+                ->addSelect('rd.codigoCeldaFk')
+                ->where("rd.codigoReunionFk = {$codigoReunion}");
+            $arReunionDetalles = $queryBuilder->getQuery()->getResult();
+            return [
+                'error' => false,
+                'reunion' => $arReunion[0],
+                'reunionDetalles' => $arReunionDetalles
+            ];
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => 'No existe la reunion'
+            ];
+        }
+    }
 }
